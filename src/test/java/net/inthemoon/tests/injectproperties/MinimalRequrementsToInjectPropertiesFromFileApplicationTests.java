9b017ca8b6917c580cede2b80bd1c520d3d10b0d
dims12@mail.ru <dims12@mail.ru>
2016-03-24 16:34:33 +0300

    Initial commit

---
+++ src/test/java/net/inthemoon/tests/injectproperties/MinimalRequrementsToInjectPropertiesFromFileApplicationTests.java
@@ -0,0 +1,16 @@
+package net.inthemoon.tests.injectproperties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MinimalRequrementsToInjectPropertiesFromFileApplication.class)
+public class MinimalRequrementsToInjectPropertiesFromFileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
